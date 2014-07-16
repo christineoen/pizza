d66b50225aa828393346ef6b05da57f47a359abe
vagrant <vagrant@student-dev-box>
2014-07-16 10:20:27 -0500

    completed pizza project

--- pizza.rb
+++ pizza.rb
@@ -1,10 +1,17 @@
 class Pizza
+  attr_accessor :toppings
+
+  def initialize(toppings = [Topping.new('cheese', vegetarian: true)])
+    @toppings = toppings
+  end
+
 end
 
 class Topping
-  attr_accessor :name
-  
-  def initialize(name)
+  attr_accessor :name, :vegetarian
+
+  def initialize(name, vegetarian: false)
     @name = name
+    @vegetarian = vegetarian
   end
 end
--- spec/pizza_spec.rb
+++ spec/pizza_spec.rb
@@ -4,6 +4,27 @@ describe Pizza do
   it "exists" do
     expect(Pizza).to be_a(Class)
   end
+
+  describe '.initialize' do
+    it 'records all of the toppings' do
+      toppings = [
+        Topping.new('mushrooms', vegetarian:true),
+        Topping.new('pepperoni')
+      ]
+      pizza = Pizza.new(toppings)
+
+      expect(pizza.toppings).to eq(toppings)
+    end
+
+    it 'defaults the toppings to cheese only, if the pizza has no toppings' do
+      pizza = Pizza.new
+
+      expect(pizza.toppings.size).to eq(1)
+      expect(pizza.toppings.first.name).to eq('cheese')
+    end
+
+  end
+
 end
 
 describe Topping do
